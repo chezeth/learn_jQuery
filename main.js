7c776e0941f516ce77d9c4dd112b2c6fffd98412
Yurii <sogdrums@gmail.com>
2018-06-07 23:07:49 +0300

    add button and change text

---
+++ main.js
@@ -0,0 +1,18 @@
+//1st mehtod
+$("h1").css("color","red");  
+
+//2nd method
+    // $(document).ready(function () {  
+    //     $("h1").css("color","red");
+    // })
+
+//3rd method
+    // $(function(){
+    //     $("h1").css("color","red");
+    // })
+
+$('h1').html('new text');
+
+$('#btn_start').click(function(){
+    $('#message').html('Message')
+})
