ba758fa29a1619485e0cdabaeaf007cd60eb10d0
Yurii <sogdrums@gmail.com>
2018-06-10 15:04:23 +0300

    created lesson 2

--- main.js
+++ main.js
@@ -1,33 +1,37 @@
-//1st mehtod
-$("h1").css("color","red");  
+// ---lesson 1
 
-//2nd method
-    // $(document).ready(function () {  
-    //     $("h1").css("color","red");
-    // })
+// //1st mehtod
+// $("h1").css("color","red");  
 
-//3rd method
-    // $(function(){
-    //     $("h1").css("color","red");
-    // })
+// //2nd method
+//     // $(document).ready(function () {  
+//     //     $("h1").css("color","red");
+//     // })
 
-// $('h1').html('new text');
+// //3rd method
+//     // $(function(){
+//     //     $("h1").css("color","red");
+//     // })
 
-$('#btn_start').click(function(){
-    $('#message')
-        .html('Message')
-        .css('background-color','green')
-        .parent()
-        .css('background-color','gold')
-        .width(150)
-        .height(80)
-});
+// // $('h1').html('new text');
 
-//так не работает
-// $('#btn_start').click(
-//     $('#message').html('Message')
-// )
+// $('#btn_start').click(function(){
+//     $('#message')
+//         .html('Message')
+//         .css('background-color','green')
+//         .parent()
+//         .css('background-color','gold')
+//         .width(150)
+//         .height(80)
+// });
 
-$('#btn_reset').click(function() {
-    location.reload()
-});
+// //так не работает
+// // $('#btn_start').click(
+// //     $('#message').html('Message')
+// // )
+
+// $('#btn_reset').click(function() {
+//     location.reload()
+// });
+
+//---lesson 2
